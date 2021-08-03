a3fdeadb8779b1acf28dc12d387dd8f3a1dead70
jeffreyrosenbluth <jeffrey.rosenbluth@gmail.com>
2021-08-02 19:20:47 -0600

    quadsub

--- src/bin/quadsub.rs
+++ src/bin/quadsub.rs
@@ -1,8 +1,8 @@
 use wassily::prelude::*;
 use wassily::skia::Canvas;
 
-const WIDTH: u32 = 1200;
-const HEIGHT: u32 = 800;
+const WIDTH: u32 = 8100;
+const HEIGHT: u32 = 6075;
 
 fn main() {
     let mut canvas = Canvas::new(WIDTH, HEIGHT);
@@ -15,34 +15,44 @@ fn main() {
     );
     let mut qs = vec![quad];
     let mut prng = Rand::new(87654321);
-    let n = 10;
+    let n = 12;
     for _ in 0..n {
         qs = subdivide_vec(
             &qs,
             |q| q.best_dir(),
             || {
-                let a = prng.rand_normal(0.5, 0.2);
+                let a = prng.rand_normal(0.5, 0.1);
                 let a = a.clamp(0.0, 1.0);
-                let b = prng.rand_normal(0.5, 0.2);
+                let b = prng.rand_normal(0.5, 0.1);
                 let b = b.clamp(0.0, 1.0);
                 (a, b)
             },
         );
     }
-    let mut palette = Palette::with_img("orange.png", Some(2usize.pow(n+1)));
-    palette.jiggle(0, 0.05);
-    palette.sort_by_hue();
-    dbg!(palette.len());
-    // palette.rotate_hue(90.0);
-    let mut iter = palette.into_iter().cycle();
-    qs.sort();
+    // // let mut palette = Palette::with_img("church.png", Some(2usize.pow(n)));
+    // // palette.jiggle(0, 0.05);
+    // // palette.sort_by_hue();
+    // // let mut iter = palette.into_iter().cycle();
+    // qs.sort();
     // qs.shuffle(&mut prng.rng);
+    let img = image::open("fireweed.png").unwrap();
     for q in qs {
-        let c = iter.next().unwrap();
+        let (q1, _) = q.subdivide(|q| q.best_dir(), || (0.75, 0.75));
+        let (_, q2) = q.subdivide(|q| q.best_dir(), || (0.25, 0.25));
+        let c = get_color(&img, WIDTH as f32, HEIGHT as f32, q.bl).set_opacity(0.75);
         ShapeBuilder::new()
-            .points(&q.to_vec())
+            .points(&q1.to_vec())
             .fill_color(c)
-            .stroke_color(c)
+            // .stroke_color(c)
+            .no_stroke()
+            .build()
+            .draw(&mut canvas);
+        let d = get_color(&img, WIDTH as f32, HEIGHT as f32, q.tr).set_opacity(0.75);
+        ShapeBuilder::new()
+            .points(&q2.to_vec())
+            .fill_color(d)
+            // .stroke_color(d)
+            .no_stroke()
             .build()
             .draw(&mut canvas);
     }
--- src/kolor.rs
+++ src/kolor.rs
@@ -1,3 +1,5 @@
+//! Utilities to manage colors and palettes.
+
 use crate::{base::RGBA, prelude::Point};
 use color_thief::{get_palette, ColorFormat};
 use image::{DynamicImage, GenericImageView};
@@ -5,25 +7,31 @@ use num_traits::AsPrimitive;
 use palette::{
     rgb::{Rgb, Rgba},
     white_point::D65,
-    Alpha, ConvertInto, Lab, LabHue, Laba, Lcha, Srgb, Srgba,
+    Alpha, IntoColor, Lab, LabHue, Laba, Lcha, Srgb, Srgba,
 };
 use rand::prelude::*;
 use rand_distr::Normal;
 use rand_pcg::Pcg64;
 use std::{ops::Index, ops::IndexMut, path::Path, usize};
 
+/// Perturb a `RGBA` value.
 pub struct Jiggle {
     rng: Pcg64,
     normal: Normal<f32>,
 }
 
 impl Jiggle {
+    /// `std_dev` as percentage of color channel, 0.01 to 0.2 works well.
+    /// Larger standard deviations will produce colors very far from the input
+    /// color.
     pub fn new(seed: u64, std_dev: f32) -> Self {
         let rng = Pcg64::seed_from_u64(seed);
         let normal = Normal::new(0.0, std_dev).unwrap();
         Self { rng, normal }
     }
 
+    /// Perturb the r, g, b channels of an `RGBA` color using a normal distribution.
+    /// The value is clamped to [0, 1] and applied as a percentage.
     pub fn jiggle(&mut self, color: RGBA) -> RGBA {
         let (r, g, b, a) = color.as_f32s();
         RGBA::rgba(
@@ -36,31 +44,36 @@ impl Jiggle {
 }
 
 impl RGBA {
+    /// Black with opacity alpha [0.0, 1.0].
     pub fn black(alpha: f32) -> Self {
         Self::rgba(0.0, 0.0, 0.0, alpha)
     }
 
+    /// White with opacity alpha [0.0, 1.0].
     pub fn white(alpha: f32) -> Self {
         Self::rgba(1.0, 1.0, 1.0, alpha)
     }
 
+    /// Red with opacity alpha [0.0, 1.0].
     pub fn red(alpha: f32) -> Self {
         Self::rgba(1.0, 0.0, 0.0, alpha)
     }
 
+    /// Green with opacity alpha [0.0, 1.0].
     pub fn green(alpha: f32) -> Self {
         Self::rgba(0.0, 1.0, 0.0, alpha)
     }
 
+    /// Blue with opacity alpha [0.0, 1.0].
     pub fn blue(alpha: f32) -> Self {
         Self::rgba(0.0, 0.0, 1.0, alpha)
     }
 
-    /// Convert a 'RGBA' to a palette Lcha.
+    /// Convert a `RGBA` to a [palette::Lcha].
     pub fn lcha(self) -> Lcha<D65> {
         let (r, g, b, a) = self.as_f32s();
         let srgb: Alpha<Rgb, f32> = Rgba::new(r, g, b, a);
-        srgb.into()
+        srgb.into_color()
     }
 }
 
@@ -69,52 +82,7 @@ impl From<image::Rgba<u8>> for RGBA {
         RGBA::rgba8(p.0[0], p.0[1], p.0[2], p.0[3])
     }
 }
-
-/// Each color channel's (red, green, and blue) value is a function of some
-/// angle (theta). c(theta) = a + b * cos(freq * theta + phase).
-#[derive(Debug, Clone, Copy)]
-pub struct CosChannel {
-    pub a: f32,
-    pub b: f32,
-    pub freq: f32,  // radians
-    pub phase: f32, // radians
-}
-
-impl CosChannel {
-    pub fn new(a: f32, b: f32, phase: f32) -> Self {
-        let freq = 1.0;
-        Self { a, b, freq, phase }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct CosColor {
-    r: CosChannel,
-    g: CosChannel,
-    b: CosChannel,
-}
-
-impl CosColor {
-    pub fn new(r: CosChannel, g: CosChannel, b: CosChannel) -> Self {
-        Self { r, g, b }
-    }
-
-    pub fn cos_color(&self, theta: f32) -> RGBA {
-        let r = self.r;
-        let g = self.g;
-        let b = self.b;
-        let red = r.a + r.b * (r.freq * theta + r.phase).cos();
-        let green = g.a + g.b * (g.freq * theta + g.phase).cos();
-        let blue = b.a + b.b * (b.freq * theta + b.phase).cos();
-        RGBA::rgba(
-            red.clamp(0.0, 1.0),
-            green.clamp(0.0, 1.0),
-            blue.clamp(0.0, 1.0),
-            1.0,
-        )
-    }
-}
-
+/// A Palette of colors and functions to manage them.
 #[derive(Clone, Debug)]
 pub struct Palette {
     pub colors: Vec<RGBA>,
@@ -140,10 +108,13 @@ impl Palette {
         }
     }
 
+    /// Set the seed of the random number generator used in all of the random
+    /// color functions.
     pub fn set_seed(&mut self, seed: u64) {
         self.rng = Pcg64::seed_from_u64(seed);
     }
 
+    /// The index of the color list.
     pub fn set_index(&mut self, i: usize) {
         self.current = i % self.colors.len();
     }
@@ -179,6 +150,7 @@ impl Palette {
         Self::new(cs)
     }
 
+    /// Create a palette of colors using the [color_thief] package.
     pub fn steal<T: AsRef<Path>>(path: T, max_colors: u8) -> Self {
         fn find_color(t: image::ColorType) -> ColorFormat {
             match t {
@@ -190,17 +162,11 @@ impl Palette {
         let img = image::open(path).expect("Could not find image file");
         let color_type = find_color(img.color());
         let palette = get_palette(img.as_bytes(), color_type, 10, max_colors).unwrap();
-        let palette = palette.into_iter().map(|c| {
-            RGBA::rgba(
-                c.r as f32 / 255.0,
-                c.g as f32 / 255.0,
-                c.b as f32 / 255.0,
-                1.0,
-            )
-        });
+        let palette = palette.into_iter().map(|c| RGBA::rgba8(c.r, c.g, c.b, 255));
         Self::new(palette.collect())
     }
 
+    /// Rotate the [palette::LabHue] of each color.
     pub fn rotate_hue(&mut self, degrees: f32) {
         self.colors = self
             .colors
@@ -209,13 +175,14 @@ impl Palette {
                 let mut l = c.lcha();
                 let hue = (l.hue.to_degrees() + degrees) % 360.0;
                 l.hue = LabHue::from_degrees(hue);
-                let rgba: Srgba = l.convert_into();
+                let rgba: Srgba = l.into_color();
                 let c = rgba.into_components();
                 RGBA::rgba(c.0, c.1, c.2, c.3)
             })
             .collect();
     }
 
+    /// Sort the colors by hue using the CIELCh color space.
     pub fn sort_by_hue(&mut self) {
         self.colors.sort_by_cached_key(|c| {
             let lcha = c.lcha();
@@ -223,6 +190,7 @@ impl Palette {
         })
     }
 
+    /// Sort the colors by chroma using the CIELCh color space.
     pub fn sort_by_chroma(&mut self) {
         self.colors.sort_by_cached_key(|c| {
             let lcha = c.lcha();
@@ -230,6 +198,7 @@ impl Palette {
         })
     }
 
+    /// Sort the colors by lightness using the CIELCh color space.
     pub fn sort_by_lightness(&mut self) {
         self.colors.sort_by_cached_key(|c| {
             let lcha = c.lcha();
@@ -237,6 +206,7 @@ impl Palette {
         })
     }
 
+    /// Sort the colors by alpha(opacity) using the CIELCh color space.
     pub fn sort_by_alpha(&mut self) {
         self.colors.sort_by_cached_key(|c| {
             let lcha = c.lcha();
@@ -244,40 +214,47 @@ impl Palette {
         })
     }
 
+    /// Choose a color from the palette at random.
     pub fn rand_color(&mut self) -> RGBA {
         self.colors[self.rng.gen_range(0..self.colors.len())]
     }
 
+    /// Generate a random opaque color independent of the `Palette` colors.
     pub fn rand_lab(&mut self) -> RGBA {
         let l: f32 = self.rng.gen_range(0.0..100.0);
         let a: f32 = self.rng.gen_range(-128.0..127.0);
         let b: f32 = self.rng.gen_range(-128.0..127.0);
-        let rgb: Srgb = Lab::new(l, a, b).convert_into();
+        let rgb: Srgb = Lab::new(l, a, b).into_color();
         let c = rgb.into_components();
         RGBA::rgba(c.0, c.1, c.2, 1.0)
     }
 
+    /// Generate a random color and random opacity independent of the `Palette` colors.
     pub fn rand_laba(&mut self) -> RGBA {
         let l: f32 = self.rng.gen_range(0.0..100.0);
         let a: f32 = self.rng.gen_range(-128.0..127.0);
         let b: f32 = self.rng.gen_range(-128.0..127.0);
         let o: f32 = self.rng.gen_range(0.0..1.0);
-        let rgba: Srgba = Laba::new(l, a, b, o).convert_into();
+        let rgba: Srgba = Laba::new(l, a, b, o).into_color();
         let c = rgba.into_components();
         RGBA::rgba(c.0, c.1, c.2, c.3)
     }
 
+    /// Perturb the colors in the palette using a normal distrtibution with
+    /// standard deviation `std_dev` considered as a percentage.
     pub fn jiggle(&mut self, seed: u64, std_dev: f32) {
         let mut j = Jiggle::new(seed, std_dev);
         let cs: Vec<RGBA> = self.colors.iter().map(|c| j.jiggle(*c)).collect();
         self.colors = cs;
     }
 
+    /// The number of colors in the `Palette`.
     pub fn len(&self) -> usize {
         self.colors.len()
     }
 }
 
+/// Allow colors to be accessed as if `Palette` was an array, e.g. `palette[42]`.
 impl Index<usize> for Palette {
     type Output = RGBA;
 
@@ -286,6 +263,7 @@ impl Index<usize> for Palette {
     }
 }
 
+/// Allow colors to be accessed and mutated as if `Palette` was an array, e.g. `palette[42] = GRAY`.
 impl IndexMut<usize> for Palette {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.colors[index]
@@ -302,13 +280,57 @@ impl IntoIterator for Palette {
     }
 }
 
+/// Each color channel's (red, green, and blue) value is a function of some
+/// angle (theta). c(theta) = a + b * cos(freq * theta + phase).
+#[derive(Debug, Clone, Copy)]
+pub struct CosChannel {
+    pub a: f32,
+    pub b: f32,
+    pub freq: f32,  // radians
+    pub phase: f32, // radians
+}
+
+impl CosChannel {
+    pub fn new(a: f32, b: f32, phase: f32) -> Self {
+        let freq = 1.0;
+        Self { a, b, freq, phase }
+    }
+}
+
+/// [Procedural Color Palettess](https://iquilezles.org/www/articles/palettes/palettes.htm).
+#[derive(Debug, Clone, Copy)]
+pub struct CosColor {
+    r: CosChannel,
+    g: CosChannel,
+    b: CosChannel,
+}
+
+impl CosColor {
+    pub fn new(r: CosChannel, g: CosChannel, b: CosChannel) -> Self {
+        Self { r, g, b }
+    }
+
+    /// Create a procedural color as a function of the angle `theta` (radians).
+    pub fn cos_color(&self, theta: f32) -> RGBA {
+        let r = self.r;
+        let g = self.g;
+        let b = self.b;
+        let red = r.a + r.b * (r.freq * theta + r.phase).cos();
+        let green = g.a + g.b * (g.freq * theta + g.phase).cos();
+        let blue = b.a + b.b * (b.freq * theta + b.phase).cos();
+        RGBA::rgba(
+            red.clamp(0.0, 1.0),
+            green.clamp(0.0, 1.0),
+            blue.clamp(0.0, 1.0),
+            1.0,
+        )
+    }
+}
+
+/// Get a color from an image by mapping the canvas coordinates to image coordinates.
 pub fn get_color<T: AsPrimitive<f32>>(img: &DynamicImage, width: T, height: T, p: Point) -> RGBA {
-    let x = p.x * img.width() as f32 / width.as_();
-    let y = p.y * img.height() as f32 / height.as_();
-    let p = img.get_pixel(x as u32, y as u32);
-    let r = p.0[0] as f32 / 255.0;
-    let g = p.0[1] as f32 / 255.0;
-    let b = p.0[2] as f32 / 255.0;
-    let a = p.0[3] as f32 / 255.0;
-    RGBA::rgba(r, g, b, a)
+    let x = ((p.x * img.width() as f32 / width.as_()) as u32).clamp(0, img.width() - 1);
+    let y = ((p.y * img.height() as f32 / height.as_()) as u32).clamp(0, img.height() - 1);
+    let p = img.get_pixel(x, y);
+    p.into()
 }
