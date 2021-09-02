8c8b43e2d3e553fa968f104f6a18bdde21d2801a
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2021-09-02 10:42:05 -0400

    turn off antialiasing

--- src/base.rs
+++ src/base.rs
@@ -11,7 +11,9 @@ pub trait Sketch {
         self.fill_path(&rect, texture);
     }
     fn pixel(&mut self, x: f32, y: f32, color: RGBA) {
-        self.fill_rect(x, y, 1.0, 1.0, &Texture::solid_color(color));
+        let mut tex = Texture::solid_color(color);
+        tex.anti_alias = false;
+        self.fill_rect(x, y, 1.0, 1.0, &tex);
     }
     fn save<P: AsRef<std::path::Path>>(&self, path: P);
 }
