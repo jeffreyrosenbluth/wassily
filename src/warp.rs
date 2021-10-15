dc113eabe4bad84e5dd11257332774ce12695b94
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2021-10-15 14:16:48 -0400

    more warp

--- src/warp.rs
+++ src/warp.rs
@@ -1,13 +1,15 @@
 use crate::base::RGBA;
+use num_complex::Complex32;
 use crate::prelude::{get_color_clamp, point2};
 use image::DynamicImage;
 use std::rc::Rc;
 
-type DomWarp = Rc<dyn Fn(f32, f32) -> f32>;
+type DomWarp = Rc<dyn Fn(Complex32) -> Complex32>;
 
 pub enum Coord {
     Polar,
     Cartesian,
+    Absolute,
 }
 pub enum Final<'a> {
     More(Box<Warp<'a>>),
@@ -16,35 +18,34 @@ pub enum Final<'a> {
 }
 
 pub struct Warp<'a> {
-    f: DomWarp,
-    g: DomWarp,
+    dw: DomWarp,
     warp: Final<'a>,
     coord: Coord,
 }
 
 impl<'a> Warp<'a> {
-    pub fn new(f: DomWarp, g: DomWarp, warp: Final<'a>, coord: Coord) -> Self {
-        Self { f, g, warp, coord }
+    pub fn new(dw: DomWarp, warp: Final<'a>, coord: Coord) -> Self {
+        Self { dw, warp, coord }
     }
 
     pub fn with_image(
-        f: DomWarp,
-        g: DomWarp,
+        dw: DomWarp,
         img: &'a DynamicImage,
         width: f32,
         height: f32,
         coord: Coord,
     ) -> Self {
         let warp = Final::Img(img, width, height);
-        Self { f, g, warp, coord }
+        Self { dw, warp, coord }
     }
 
     pub fn get(&self, x: f32, y: f32) -> RGBA {
-        let a = (self.f)(x, y);
-        let b = (self.g)(x, y).abs();
+        let c = (self.dw)(Complex32::new(x, y));
+        let r = c.im.abs();
         let (x1, y1) = match self.coord {
-            Coord::Polar => (x + a.cos() * b, y + a.sin() * b),
-            Coord::Cartesian => (x + a, y + b),
+            Coord::Polar => (x + c.re.cos() * r, y + c.re.sin() * r),
+            Coord::Cartesian => (x + c.re, y + c.im),
+            Coord::Absolute => (c.re, c.im),
         };
         match &self.warp {
             Final::More(w) => w.get(x1, y1),
