de6c304000df0d55ca319a03049f6e26131bd921
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2021-11-03 11:10:35 -0400

    matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,15 +1,20 @@
+use num_traits::{Float, One, AsPrimitive, Zero};
+/// A zero indexed row-major matrix.
+/// Allows acces to elements of matrix A by A[i][j]
 use std::ops::{Index, IndexMut};
-
-use num_traits::{One, Zero};
 
 #[derive(Debug)]
 pub struct Matrix<T> {
     rows: usize,
     cols: usize,
-    data: Vec<T>,
+    pub data: Vec<T>,
 }
 
 impl<T> Matrix<T> {
+    pub fn new<U: AsPrimitive<usize>>(rows: U, cols: U, data: Vec<T>) -> Self {
+        Self { rows: rows.as_(), cols: cols.as_(), data }
+    }
+
     pub fn rows(&self) -> usize {
         self.rows
     }
@@ -22,17 +27,17 @@ impl<T> Matrix<T> {
         row * self.cols + col
     }
 
-    pub fn generate<F>(rows: usize, cols: usize, generator: F) -> Self
+    pub fn generate<F, U: AsPrimitive<usize>>(rows: U, cols: U, generator: F) -> Self
     where
         F: Fn(usize, usize) -> T,
     {
         let mut data: Vec<T> = vec![];
-        for r in 0..rows {
-            for c in 0..cols {
+        for r in 0..rows.as_() {
+            for c in 0..cols.as_() {
                 data.push(generator(r, c))
             }
         }
-        Matrix { rows, cols, data }
+        Matrix { rows: rows.as_(), cols: cols.as_(), data }
     }
 
     pub fn get_ref(&self, row: usize, col: usize) -> Option<&T> {
@@ -77,9 +82,9 @@ impl<T> Matrix<T>
 where
     T: Zero + Clone,
 {
-    pub fn zeros(rows: usize, cols: usize) -> Self {
-        let data = vec![T::zero(); rows * cols];
-        Self { rows, cols, data }
+    pub fn zeros<U: AsPrimitive<usize>>(rows: U, cols: U) -> Self {
+        let data = vec![T::zero(); rows.as_() * cols.as_()];
+        Self { rows: rows.as_(), cols: cols.as_(), data }
     }
 }
 
@@ -87,9 +92,35 @@ impl<T> Matrix<T>
 where
     T: One + Clone,
 {
-    pub fn ones(rows: usize, cols: usize) -> Self {
-        let data = vec![T::one(); rows * cols];
-        Self { rows, cols, data }
+    pub fn ones<U: AsPrimitive<usize>>(rows: U, cols: U) -> Self {
+        let data = vec![T::one(); rows.as_() * cols.as_()];
+        Self { rows: rows.as_(), cols: cols.as_(), data }
+    }
+}
+
+impl<T> Matrix<T>
+where
+    T: Float,
+{
+    pub fn convolve(&self, kernel: &Matrix<T>) -> Matrix<T> {
+        let mut m: Matrix<T> = Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: self.data.clone(),
+        };
+        let k = kernel.rows / 2;
+        for i in k..self.rows - k {
+            for j in k..self.cols - k {
+                let mut acc = T::zero();
+                for r in 0..kernel.rows {
+                    for c in 0..kernel.cols {
+                        acc = acc + self[i - k + r][j - k + c] * kernel[r][c];
+                    }
+                }
+                m[i][j] = acc;
+            }
+        }
+        m
     }
 }
 
@@ -114,30 +145,30 @@ mod tests {
     #[test]
     fn gen_test() {
         let m = Matrix::generate(2, 3, |i, j| (i, j));
-        assert_eq!(m.data, vec![(0,0), (0,1), (0,2), (1,0), (1,1), (1,2)]);
+        assert_eq!(m.data, vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]);
     }
 
     #[test]
     fn get_test() {
         let m = Matrix::generate(2, 3, |i, j| (i, j));
-        assert_eq!(m.get(1,1), Some((1,1)));
-        assert_eq!(m.get(2,1), None);
-        assert_eq!(m.get(0,3), None);
+        assert_eq!(m.get(1, 1), Some((1, 1)));
+        assert_eq!(m.get(2, 1), None);
+        assert_eq!(m.get(0, 3), None);
     }
 
     #[test]
     fn get_ref_test() {
         let m = Matrix::generate(2, 3, |i, j| (i, j));
-        assert_eq!(m.get_ref(1,1), Some(&(1,1)));
-        assert_eq!(m.get_ref(2,1), None);
-        assert_eq!(m.get_ref(0,3), None);
+        assert_eq!(m.get_ref(1, 1), Some(&(1, 1)));
+        assert_eq!(m.get_ref(2, 1), None);
+        assert_eq!(m.get_ref(0, 3), None);
     }
 
     #[test]
     fn put_test() {
         let mut m = Matrix::generate(2, 3, |i, j| (i, j));
-        assert_eq!(m.put(1,1, (5,5)), true);
-        assert_eq!(m.get(1,1), Some((5,5)));
+        assert_eq!(m.put(1, 1, (5, 5)), true);
+        assert_eq!(m.get(1, 1), Some((5, 5)));
     }
 
     #[test]
@@ -149,13 +180,22 @@ mod tests {
     #[test]
     fn index_test() {
         let m = Matrix::generate(2, 3, |i, j| (i, j));
-        assert_eq!(m[1][1], (1,1));
+        assert_eq!(m[1][1], (1, 1));
     }
 
     #[test]
     fn indexmut_test() {
         let mut m = Matrix::generate(2, 3, |i, j| (i, j));
-        m[1][1] = (5,5);
-        assert_eq!(m[1][1], (5,5));
+        m[1][1] = (5, 5);
+        assert_eq!(m[1][1], (5, 5));
+    }
+
+    #[test]
+    fn convolve_test() {
+        let m = Matrix::<f32>::ones(5, 5);
+        let k = Matrix::<f32>::ones(3, 3);
+        let c = m.convolve(&k);
+        assert_eq!(c[0][0], 1.0);
+        assert_eq!(c[1][1], 9.0);
     }
 }
