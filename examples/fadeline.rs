95ea20d422a2ff0963c34ff3609ae3d5af99e279
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2024-10-29 13:25:20 -0400

    fadeline

--- examples/fadeline.rs
+++ examples/fadeline.rs
@@ -1,52 +1,94 @@
 use wassily::prelude::*;
 
 fn main() {
-    let width = 1080;
-    let height = 1080;
-    let linecolor = *WHITE;
-    let mut canvas = Canvas::new(width, height);
-    canvas.fill(rgb8(0, 5, 25));
-    let w = width as f32;
-    let h = height as f32;
-    let spacing = 3.0;
-    let thickness = 0.20;
+    let img = open("/Users/jeffreyrosenbluth/Desktop/Gino_Severini.jpg").unwrap();
+    let img_large = img.resize(
+        5 * img.width(),
+        5 * img.height(),
+        image::imageops::FilterType::Lanczos3,
+    );
+    let blurred_img = img_large.fast_blur(100.0);
+    let mut canvas = Canvas::from_image(&blurred_img);
+
+    let width = canvas.width();
+    let height = canvas.height();
+    let linecolor = *BLACK;
+    let w = canvas.w_f32();
+    let h = canvas.h_f32();
+    let spacing = 15.0;
+    let thickness = 0.50;
+    let subdivisions = 75;
+    let min_opacity = 0.15;
+    let max_opacity = 1.0;
     let mut i = spacing;
-    while i < w {
-        let p0 = pt(i, 0);
-        let p1 = pt(0, i);
-        let mut fl = FadeLine::new(p0, p1, 98731 + i as u64)
-            .subdivisions(2 + 50 * i as u32 / (h * h + w * w).sqrt() as u32)
+
+    // let mut canvas = Canvas::new(width, height);
+    // canvas.fill(rgb8(255, 255, 255));
+
+    while i < h {
+        let v0 = pt(0, i);
+        let v1 = pt(width, i);
+        let mut fl = FadeLine::new(v0, v1, 98731 + i as u64)
+            .subdivisions(subdivisions)
             .thickness(thickness)
-            .color(linecolor);
-        fl.draw(&mut canvas);
-        let p0 = pt(i, height);
-        let p1 = pt(width, i);
-        let mut fl = FadeLine::new(p0, p1, 8317 + i as u64)
-            .subdivisions(2 + 50 * (w - i) as u32 / (h * h + w * w).sqrt() as u32)
-            .thickness(thickness)
+            .min_opacity(min_opacity)
+            .max_opacity(max_opacity)
             .color(linecolor);
         fl.draw(&mut canvas);
         i += spacing;
     }
     i = spacing;
     while i < w {
-        let p0 = pt(i, 0);
-        let p1 = pt(width, w - i);
-        let mut fl = FadeLine::new(p0, p1, 137 + i as u64)
-            .subdivisions(2 + 50 * (w - i) as u32 / (h * h + w * w).sqrt() as u32)
+        let v0 = pt(i, 0);
+        let v1 = pt(i, height);
+        let mut fl = FadeLine::new(v0, v1, 98731 + i as u64)
+            .subdivisions(subdivisions)
             .thickness(thickness)
-            .color(linecolor);
-        fl.draw(&mut canvas);
-        let p0 = pt(i, height);
-        let p1 = pt(0, h - i);
-        let mut fl = FadeLine::new(p0, p1, 137 + i as u64)
-            .subdivisions(2 + 50 * i as u32 / (h * h + w * w).sqrt() as u32)
-            .thickness(thickness)
+            .min_opacity(min_opacity)
+            .max_opacity(max_opacity)
             .color(linecolor);
         fl.draw(&mut canvas);
         i += spacing;
     }
-    canvas.save_png("./denim.png");
+
+    // --- Diagonal lines --------------------------------------------------------
+
+    // while i < w {
+    //     let p0 = pt(i, 0);
+    //     let p1 = pt(0, i);
+    //     let mut fl = FadeLine::new(p0, p1, 98731 + i as u64)
+    //         .subdivisions(2 + 50 * i as u32 / (h * h + w * w).sqrt() as u32)
+    //         .thickness(thickness)
+    //         .color(linecolor);
+    //     fl.draw(&mut canvas);
+    //     let p0 = pt(i, height);
+    //     let p1 = pt(width, i);
+    //     let mut fl = FadeLine::new(p0, p1, 8317 + i as u64)
+    //         .subdivisions(2 + 50 * (w - i) as u32 / (h * h + w * w).sqrt() as u32)
+    //         .thickness(thickness)
+    //         .color(linecolor);
+    //     fl.draw(&mut canvas);
+    //     i += spacing;
+    // }
+    // i = spacing;
+    // while i < w {
+    //     let p0 = pt(i, 0);
+    //     let p1 = pt(width, w - i);
+    //     let mut fl = FadeLine::new(p0, p1, 137 + i as u64)
+    //         .subdivisions(2 + 50 * (w - i) as u32 / (h * h + w * w).sqrt() as u32)
+    //         .thickness(thickness)
+    //         .color(linecolor);
+    //     fl.draw(&mut canvas);
+    //     let p0 = pt(i, height);
+    //     let p1 = pt(0, h - i);
+    //     let mut fl = FadeLine::new(p0, p1, 137 + i as u64)
+    //         .subdivisions(2 + 50 * i as u32 / (h * h + w * w).sqrt() as u32)
+    //         .thickness(thickness)
+    //         .color(linecolor);
+    //     fl.draw(&mut canvas);
+    //     i += spacing;
+    // }
+    canvas.save_png("./fade.png");
 }
 
 #[cfg(test)]
